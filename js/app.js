cc0e9fd3be1e9dddfb880d0ea6be20a471b32305
Pavel <pavel.khatkevich2000@gmail.com>
2021-01-22 13:54:33 +0300

    added a animations

--- js/app.js
+++ js/app.js
@@ -13,5 +13,51 @@ $(function() {
     $(this).toggleClass('is-active');
     $('.main-menu').slideToggle();  
     $('body').toggleClass('ov-h');
-	});
+  });
+  
+  wow = new WOW({
+    boxClass:     'wow',      // default
+    animateClass: 'animate__animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  })
+  wow.init();
+
+  $(document).on('click', 'a', function(e) {
+    const link = $(this).attr('href');
+
+    if (link[0] !== '#') {
+      e.preventDefault();
+
+      closeMenu()
+
+      $('.wrapper').removeClass('animate__fadeInUp animate__slower');
+      $('.wrapper').addClass('animate__fadeOutUp animate__slow');
+      
+      $('.wrapper').on('animationend webkitAnimationEnd oAnimationEnd', function () {
+
+        $(location).attr( 'href', link );
+
+      });
+    }
+  });
+
+  $('.wrapper').on('animationend webkitAnimationEnd oAnimationEnd', function () {
+    $('body').removeClass('ov-h');
+    return;
+  });
+
+  $('.list-social__li').each(function(index) {
+    $(this).css('animationDelay', index / 5 + 's');
+  });
+
+
+  function closeMenu() {
+    if ($(window).width() < 992) {
+      $('.hamburger').removeClass('is-active');
+      $('.main-menu').slideUp();  
+      $('body').removeClass('ov-h');
+    }
+  }
 });
